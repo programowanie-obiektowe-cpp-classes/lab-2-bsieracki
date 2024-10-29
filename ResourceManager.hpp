c6405cccdcf45df30af58bf8e2fd33c650c1963e
Bartosz Sieracki <105609470+bsieracki@users.noreply.github.com>
2024-10-29 21:00:50 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,36 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource;
+
+public:
+    ResourceManager() : resource(new Resource()){};
+    ResourceManager(const ResourceManager& resourceManager) : resource(new Resource(*resourceManager.resource)){};
+    ResourceManager& operator=(const ResourceManager& resourceManager)
+    {
+        if (this != &resourceManager) {
+            delete resource;
+            resource = new Resource(*resourceManager.resource);
+        }
+        return *this;
+    };
+    ResourceManager(ResourceManager&& resourceManager) : resource(resourceManager.resource)
+    {
+        resourceManager.resource = nullptr;
+    }
+    ResourceManager& operator=(ResourceManager&& resourceManager)
+    {
+        if (this != &resourceManager) {
+            delete resource;
+            resource = resourceManager.resource;
+            resourceManager.resource = nullptr;
+        }
+        return *this;
+    }
+    ~ResourceManager()
+    {
+        delete resource;
+    };
+    double get() const { return resource->get(); }
 };
